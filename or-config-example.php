aeb6c165a68505ef59dea7d56dfb6ee9cd7498ed
ryanfmurphy <ryanfmurphy@gmail.com>
2016-06-13 19:20:47 -0500

    new or-config-example.php

--- or-config-example.php
+++ or-config-example.php
@@ -1,6 +1,35 @@
 <?php
-    define('DB_HOST', '<YOUR_DB_HOST_HERE>');
-    define('DB_USER', '<YOUR_DB_USER_HERE>');
-    define('DB_PASSWORD', '<YOUR_DB_PW_HERE>');
-    define('DB_NAME', '<YOUR_DB_HERE>');
-	define('URI_PREFIX', '/');
+# UNDER CONSTRUCTION - transitioning from consts to vars
+# bear with me! :)
+#   ~ 2016-06-13 RM
+
+{ # old config style
+    define('DB_HOST', '<your-host-here>');
+
+    define('DB_USER', '<your-db-user-here>');
+    define('DB_PASSWORD', '<your-password-here>');
+    define('DB_NAME', '<your-database-name-here>');
+    define('DB_TYPE', '<mysql-pgsql-sqlite>');
+
+	define('URI_PREFIX', '/ormrouter');
+}
+
+{ # new config style
+    $db_user = '<your-db-user-here>';
+    $db_password = '<your-password-here>';
+    $db_name = '<your-database-name-here>';
+    $db_type = '<mysql-pgsql-sqlite>';
+    #$db_port = '<port>';
+
+    # not used yet
+    #$uri_prefix = '/ormrouter';
+}
+
+{ # postgres-specific options
+    # $search_path = 'schema1, schema2, etc';
+}
+
+{ # cosmetic / UI options
+    $background = 'dark'; # vs 'light', the default
+}
+?>
