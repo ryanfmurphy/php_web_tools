445510da2e75a58ee8db0a5ffaf0cf3c04923e70
ryanfmurphy <ryanfmurphy@gmail.com>
2016-07-03 00:51:11 -0500

    simplify index: don't need 404 check
    the router will automatically 404 if route isn't found
    
    also removed commented db_viewer dependency

--- index.php
+++ index.php
@@ -2,11 +2,6 @@
 # Ryan Murphy Feb 2016
 # kick off router for /api/* URIs
 
-{ #todo make self-sufficient, don't depend on DbViewer
-    # we need setDbSearchPath
-    #require_once('/Users/murftown/webroot/db_viewer/init.php');
-}
-
 { # setup
     # gives us $ControllerClass
     require_once('util/includes.php');
@@ -17,10 +12,6 @@
 
 { # route the URL to the action
     $route = $ControllerClass::check_route();
-    if (!$route) {
-        $route = $ControllerClass::action_404();
-    }
-
     $ControllerClass::do_route($route);
 }
 
