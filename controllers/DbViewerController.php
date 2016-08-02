639b0c51c546d8b9c806ecfaa9287ffdb2612502
ryanfmurphy <ryanfmurphy@gmail.com>
2016-08-02 03:49:10 -0500

    fix paths in DbViewerController

--- controllers/DbViewerController.php
+++ controllers/DbViewerController.php
@@ -6,11 +6,11 @@ class DbViewerController extends Controller {
     }
 
     public static function action_query_id_in() {
-        require_once('views/example/query_id_in.php');
+        require_once('db_viewer/query_id_in.php');
     }
 
     public static function action_rows_with_field_vals() {
-        require_once('views/example/rows_with_field_vals.php');
+        require_once('db_viewer/rows_with_field_vals.php');
     }
 
     public static function action_style_DOT_css() {
