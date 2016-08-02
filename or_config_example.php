4a6336203c8fe00f9484cc88c465000908871316
ryanfmurphy <ryanfmurphy@gmail.com>
2016-08-01 20:07:20 -0500

    make classes/ and controllers/ symlinks
    to within the new /orm_router subtree

--- or_config_example.php
+++ or_config_example.php
@@ -1,18 +1,1 @@
-<?php
-{ # orm_router specific vars
-    define('URI_PREFIX', '/orm_router');
-    $ControllerClass = 'ExampleController';
-}
-
-{ # new config style
-    $db_user = '<your-db-user-here>';
-    $db_password = '<your-password-here>';
-    $db_name = '<your-database-name-here>';
-    $db_type = '<mysql-pgsql-sqlite>';
-    #$db_port = '<port>';
-}
-
-{ # postgres-specific options
-    # $search_path = 'schema1, schema2, etc';
-}
-?>
+orm_router/or_config_example.php
